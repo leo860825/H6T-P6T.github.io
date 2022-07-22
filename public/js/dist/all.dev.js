3d401567f4321a19b44d3acf71a91f14b740c925
leo860825 <107459223+leo860825@users.noreply.github.com>
2022-07-22 12:49:30 +0800

    0722

--- public/js/dist/all.dev.js
+++ public/js/dist/all.dev.js
@@ -6,7 +6,7 @@ function openNav() {
   console.log('點按鈕打開主導覽');
 
   if (document.getElementById('primary-navigation').classList.contains('active') == true) {
-    // 假如主導覽有active類別時執行
+    // 假如主導覽有active類別時執行  
     document.getElementById('primary-navigation').classList.remove('active');
     document.getElementById('btn-nav-toggle').textContent = 'menu';
   } else {
