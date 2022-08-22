388bf9107bb4b0b3589b920493491995c10f51e8
leo860825 <107459223+leo860825@users.noreply.github.com>
2022-08-22 13:50:35 +0800

    0822

--- public/js/all.js
+++ public/js/all.js
@@ -14,3 +14,31 @@ function openNav(){
     }// openNav if end
     
 } //openNav end
+
+
+// 捲軸事件
+
+// 定義捲軸高變數
+let offsetTop
+let product1Top = $('#logocenter,.h2under,#message,.loremh2').offset().top 
+$(window).scroll(function(){
+    offsetTop = $(window).scrollTop()
+    // console.log( '捲軸高', offsetTop )
+    // console.log('product1',product1Top)
+//    if( offsetTop >=  product1Top){
+//         console.log('test')
+//    }
+
+
+    // 捲軸高度>=900，會出現page-top的按鈕
+    if( offsetTop >= 900){
+        $('.page-top').addClass('active')
+    }else{
+        $('.page-top').removeClass('active')
+    }
+})//scroll end
+
+// 點按page-top回到最上方
+$('.page-top').on('click', function(){
+    $('html, body').animate({ scrollTop: 0 })
+})//.page-top
